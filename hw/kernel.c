5dadc5cf11001a267671fd3398fd4cfd4b29fcc2
Sebastian Wicki <gandro@gmx.net>
2015-06-30 21:20:57 +0200

    Add `hw` gluecode from rumprun
    Currently needs GNU make to compile.

---
+++ hw/kernel.c
@@ -0,0 +1,90 @@
+/*-
+ * Copyright (c) 2014 Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <bmk/types.h>
+#include <bmk/multiboot.h>
+#include <bmk/kernel.h>
+
+#include <bmk-core/core.h>
+#include <bmk-core/string.h>
+#include <bmk-core/memalloc.h>
+#include <bmk-core/pgalloc.h>
+#include <bmk-core/platform.h>
+#include <bmk-core/printf.h>
+#include <bmk-core/queue.h>
+#include <bmk-core/sched.h>
+
+void
+bmk_platform_block(bmk_time_t until)
+{
+	int s = bmk_spldepth;
+
+	/* enable interrupts around the sleep */
+	if (bmk_spldepth) {
+		bmk_spldepth = 1;
+		spl0();
+	}
+	bmk_cpu_nanohlt();
+	if (s) {
+		splhigh();
+		bmk_spldepth = s;
+	}
+}
+
+/*
+ * splhigh()/spl0() internally track depth
+ */
+unsigned long
+bmk_platform_splhigh(void)
+{
+
+	splhigh();
+	return 0;
+}
+
+void
+bmk_platform_splx(unsigned long x)
+{
+
+	spl0();
+}
+ 
+void
+bmk_run(char *cmdline)
+{
+
+	bmk_sched_startmain(bmk_mainthread, cmdline);
+}
+
+void __attribute__((noreturn))
+bmk_platform_halt(const char *panicstring)
+{
+
+	if (panicstring)
+		bmk_printf("PANIC: %s\n", panicstring);
+	bmk_printf("halted (well, spinning ...)\n");
+	for (;;)
+		continue;
+}
---
+++ hw/multiboot.c
@@ -0,0 +1,97 @@
+/*-
+ * Copyright (c) 2014 Antti Kantee.  All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <bmk/types.h>
+#include <bmk/multiboot.h>
+#include <bmk/kernel.h>
+
+#include <bmk-core/core.h>
+#include <bmk-core/pgalloc.h>
+#include <bmk-core/printf.h>
+#include <bmk-core/string.h>
+
+#define MEMSTART 0x100000
+
+static int
+parsemem(uint32_t addr, uint32_t len)
+{
+	struct multiboot_mmap_entry *mbm;
+	unsigned long osend;
+	extern char _end[];
+	uint32_t off;
+
+	/*
+	 * Look for our memory.  We assume it's just in one chunk
+	 * starting at MEMSTART.
+	 */
+	for (off = 0; off < len; off += mbm->size + sizeof(mbm->size)) {
+		mbm = (void *)(uintptr_t)(addr + off);
+		if (mbm->addr == MEMSTART
+		    && mbm->type == MULTIBOOT_MEMORY_AVAILABLE) {
+			break;
+		}
+	}
+	if (!(off < len))
+		bmk_platform_halt("multiboot memory chunk not found");
+
+	osend = bmk_round_page((unsigned long)_end);
+	bmk_assert(osend > mbm->addr && osend < mbm->addr + mbm->len);
+
+	bmk_pgalloc_loadmem(osend, mbm->addr + mbm->len);
+
+	bmk_memsize = mbm->addr + mbm->len - osend;
+
+	return 0;
+}
+
+char bmk_multiboot_cmdline[BMK_MULTIBOOT_CMDLINE_SIZE];
+
+void
+bmk_multiboot(struct multiboot_info *mbi)
+{
+	unsigned long cmdlinelen;
+	char *cmdline;
+
+	bmk_printf_init(bmk_cons_putc, NULL);
+	bmk_core_init(BMK_THREAD_STACK_PAGE_ORDER, PAGE_SHIFT);
+
+	bmk_printf("rump kernel bare metal multibootstrap\n\n");
+
+	/* save the command line before something overwrites it */
+	cmdline = (char *)(uintptr_t)mbi->cmdline;
+	cmdlinelen = bmk_strlen(cmdline);
+	if (cmdlinelen >= BMK_MULTIBOOT_CMDLINE_SIZE)
+		bmk_platform_halt("command line too long, "
+		    "increase BMK_MULTIBOOT_CMDLINE_SIZE");
+	bmk_strcpy(bmk_multiboot_cmdline, cmdline);
+
+	if ((mbi->flags & MULTIBOOT_MEMORY_INFO) == 0)
+		bmk_platform_halt("multiboot memory info not available\n");
+
+	if (parsemem(mbi->mmap_addr, mbi->mmap_length) != 0)
+		bmk_platform_halt("multiboot memory parse failed");
+
+	bmk_intr_init();
+}
